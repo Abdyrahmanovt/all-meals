8485a1be1c74417364e21aea8242936d1d46ebc9
Abdyrahmanov Timurlan <timurlanabdyrahmanov7@gmail.com>
2021-09-02 08:16:38 +0600

    update project

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-
+import logo from './logo.png'
 const Header = () => {
     return (
         <header className='header'>
             <nav className="nav-left">
                 <Link to="/home" className="nav-link">Home</Link>
                 <Link to="/foods" className="nav-link">Foods</Link>
-                <Link to="/" className="nav-link" >Contact Us</Link>
+                <Link to="/" className="nav-link" >Contacts</Link>
             </nav>
+            <img src={logo} alt="" className='logo-header'/>
         </header>
     );
 };
